67ae998976d7f600de1d91feae4925e45119594c
Ramon de Souza <ramonsouza859@gmail.com>
2024-07-30 19:39:55 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos);
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) => 
+{
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null ? TypedResults.NotFound() : TypedResults.Ok(targetTodo);
+});
+app.MapPost("/todos", (Todo task) => 
+{
+    todos.Add(task);
+    return TypedResults.Created("/todos/{id}", task);
+});
+app.MapPut("/todos/{id}", Results<Ok<Todo>, NotFound> (int id, Todo task) => 
+{
+    int index = todos.FindIndex(t => id == t.Id);
+    if(index >= 0) {
+        todos[index] = task;
+        return TypedResults.Ok(task);
+    }else{
+        return TypedResults.NotFound();
+    }
+});
+app.MapDelete("/todos/{id}", (int id) => 
+{
+    todos.RemoveAll(t => id == t.Id);
+    return TypedResults.NoContent();
+});
+
+app.Run();
+
+public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
